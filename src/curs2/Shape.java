076fce5d516ba5d93310e2d91598cc90d54a66ce
irusu <irusu@bitdefender.com>
2021-11-22 20:14:14 +0200

    moved println outside calculate methods

--- src/curs2/Shape.java
+++ src/curs2/Shape.java
@@ -2,20 +2,20 @@ package curs2;
 
 public class Shape {
 
-	public void calculateSquareArea(int lenght) {
-		System.out.println("The area of the square is: " + lenght * lenght);
+	public int calculateSquareArea(int lenght) {
+		return lenght * lenght;
 	}
 
-	public void calculateRectangleArea(int lenght, int width) {
-		System.out.println("The area of the rectangle is: " + lenght * width);
+	public int calculateRectangleArea(int lenght, int width) {
+		return lenght * width;
 	}
 
 	public Shape(int lenght) {
-		calculateSquareArea(lenght);
+		System.out.println("The area of the square is: " + calculateSquareArea(lenght));
 	}
 
 	public Shape(int lenght, int width) {
-		calculateRectangleArea(lenght, width);
+		System.out.println("The area of the rectangle is: " + calculateRectangleArea(lenght, width));
 	}
 
 	public Shape(double radius) {
